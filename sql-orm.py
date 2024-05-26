11ca8ba864c42227044681d599f93c87e600a2a9
Maksim Popov <exactlyy@gmail.com>
2024-05-26 22:19:25 +0000

    s

--- sql-orm.py
+++ sql-orm.py
@@ -66,9 +66,24 @@ artist = session.query(Artist).filter_by(ArtistId=51).first()
 print(artist.ArtistId, artist.Name, sep=" | ")
 
 # Query 5 - select only the albums with "ArtistId" #51 on the Album table
-albums = session.query(Album).filter_by(ArtistId=51)
-for album in albums:
-    print(album.AlbumId, album.Title, albumArtistId, sep=" | ")
+#albums = session.query(Album).filter_by(ArtistId=51)
+#for album in albums:
+#    print(album.AlbumId, album.Title, albumArtistId, sep=" | ")
+
+tracks = session.query(Track).filter_by(Composer="Queen")
+for track in tracks:
+    print(
+        track.TrackId,
+        track.Name,
+        track.AlbumId,
+        track.MediaTypeId,
+        track.GenreId,
+        track.Composer,
+        track.Milliseconds,
+        track.Bytes,
+        track.UnitPrice,
+        sep=" | "
+    ) 
 
 
 
